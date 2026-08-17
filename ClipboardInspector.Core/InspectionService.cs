9ece246e68fff7e3360f619b35a9a32b8f9502ae
JoshGoolnik <joshgoolnik@hotmail.com>
2026-08-17 17:11:15 +0100

    Add project files.

---
+++ ClipboardInspector.Core/InspectionService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.PresentationCore;
+namespace ClipboardInspector.Core
+{
+    public class InspectionService
+    {
+        // Read the current contents of the clipboard
+        
+    }
+}
---
+++ ClipboardInspector.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClipboardInspector.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
